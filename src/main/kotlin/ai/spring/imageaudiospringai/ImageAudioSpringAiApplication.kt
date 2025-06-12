d9c6c95890cee9773467772a2788383b9931d489
Tomáš Bilder <tomas.bilder@apptime.cz>
2025-06-12 12:36:17 +0200

    init

---
+++ src/main/kotlin/ai/spring/imageaudiospringai/ImageAudioSpringAiApplication.kt
@@ -0,0 +1,11 @@
+package ai.spring.imageaudiospringai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImageAudioSpringAiApplication
+
+fun main(args : Array<String>) {
+    runApplication<ImageAudioSpringAiApplication>(*args)
+}
---
+++ src/test/kotlin/ai/spring/imageaudiospringai/ImageAudioSpringAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ai.spring.imageaudiospringai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImageAudioSpringAiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
